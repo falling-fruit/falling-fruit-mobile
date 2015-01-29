5b4d5bb9547a2bf21413363d6e830929ff36ecf7
The Machinist? <the.machinist.source@gmail.com>
2015-01-29 14:39:44 -0600

    updated iOS and Android builds

---
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/CordovaLib/AndroidManifest.xml \
