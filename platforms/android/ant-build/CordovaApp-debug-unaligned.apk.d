054927f5e6618e13ad399d9ba4873aa6d58bd929
The Machinist? <the.machinist.source@gmail.com>
2015-01-29 14:39:44 -0600

    updated iOS and Android builds

---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/ant-build/classes.dex \
+/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/CordovaLib/ant-build/classes.jar \
