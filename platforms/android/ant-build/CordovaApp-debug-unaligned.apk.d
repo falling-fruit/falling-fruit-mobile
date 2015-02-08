401da8edc913229704a0864dc80a1753b94342c3
caleb <caleb@zeropercent.us>
2015-02-08 16:44:26 -0700

    New build, map now displays and has points on it!

--- platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
-/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/ant-build/CordovaApp.ap_ \
-/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/ant-build/classes.dex \
-/Users/achyuta/programs/mobile-apps/falling-fruit-mobile/platforms/android/CordovaLib/ant-build/classes.jar \
+/home/caleb/Checkouts/falling-fruit-mobile/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/home/caleb/Checkouts/falling-fruit-mobile/platforms/android/ant-build/CordovaApp.ap_ \
+/home/caleb/Checkouts/falling-fruit-mobile/platforms/android/ant-build/classes.dex \
+/home/caleb/Checkouts/falling-fruit-mobile/platforms/android/CordovaLib/ant-build/classes.jar \
