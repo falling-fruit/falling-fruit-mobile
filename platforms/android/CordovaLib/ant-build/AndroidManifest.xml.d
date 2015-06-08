2907408493233b80de0e43c789a774519b62f443
ezwelty <ethan.welty@gmail.com>
2015-06-08 17:30:07 -0600

    Scary commit removing automatically generated files

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/home/caleb/Projects/falling-fruit-mobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/caleb/Projects/falling-fruit-mobile/platforms/android/CordovaLib/AndroidManifest.xml \
