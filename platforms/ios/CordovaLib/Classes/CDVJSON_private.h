82a451a90e167ae96085340b9ff5109402396fde
Rylan Bowers <rylanb@gmail.com>
2015-06-10 20:16:58 -0600

    New build files with all the platforms config updated / rebuilt.
    Should be removed from git someday.

--- platforms/ios/CordovaLib/Classes/CDVJSON_private.h
+++ platforms/ios/CordovaLib/Classes/CDVJSON_private.h
@@ -6,9 +6,9 @@
  to you under the Apache License, Version 2.0 (the
  "License"); you may not use this file except in compliance
  with the License.  You may obtain a copy of the License at
- 
+
  http://www.apache.org/licenses/LICENSE-2.0
- 
+
  Unless required by applicable law or agreed to in writing,
  software distributed under the License is distributed on an
  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -17,11 +17,15 @@
  under the License.
  */
 
-// controller title for Videos
-"Videos title" = "Videos";
-// accessibility label for recording button
-"toggle audio recording" = "toggle audio recording";
-// notification spoken by VoiceOver when timed recording finishes
-"timed recording complete" = "timed recording complete";
-// accessibility hint for display of recorded elapsed time
-"recorded time in minutes and seconds" = "recorded time in minutes and seconds";
+@interface NSArray (CDVJSONSerializingPrivate)
+- (NSString*)cdv_JSONString;
+@end
+
+@interface NSDictionary (CDVJSONSerializingPrivate)
+- (NSString*)cdv_JSONString;
+@end
+
+@interface NSString (CDVJSONSerializingPrivate)
+- (id)cdv_JSONObject;
+- (id)cdv_JSONFragment;
+@end
--- platforms/ios/Falling Fruit/Classes/MainViewController.h
+++ platforms/ios/Falling Fruit/Classes/MainViewController.h
@@ -19,7 +19,7 @@
 
 //
 //  MainViewController.h
-//  FallingFruit
+//  Falling Fruit
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
